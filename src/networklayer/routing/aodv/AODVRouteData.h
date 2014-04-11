d38d9ddcb74cfdf6c5cb6ccb6ea3e02e462efe8f
Zoltan Bojthe <zbojthe@omnetpp.org>
2014-04-11 12:30:10 +0200

    AODVRouteData: << info operator: optimisations
    do not make a copy of precursorList
    ++iter instead of iter++
    uses !empty() instead of size()>0

--- src/networklayer/routing/aodv/AODVRouteData.h
+++ src/networklayer/routing/aodv/AODVRouteData.h
@@ -64,19 +64,19 @@ class INET_API AODVRouteData : public cObject
 
 inline std::ostream& operator<<(std::ostream& out, const AODVRouteData * data)
 {
-    out << " isActive = " << data->isActive();
-    out << ", hasValidDestNum = " << data->hasValidDestNum();
-    out << ", destNum = " << data->getDestSeqNum();
-    out << ", lifetime = " << data->getLifeTime();
+    out << " isActive = " << data->isActive()
+        << ", hasValidDestNum = " << data->hasValidDestNum()
+        << ", destNum = " << data->getDestSeqNum()
+        << ", lifetime = " << data->getLifeTime();
 
-    std::set<Address> preList = data->getPrecursorList();
+    const std::set<Address> &preList = data->getPrecursorList();
 
-    if (preList.size() > 0)
+    if (!preList.empty())
     {
         out << ", precursor list: ";
-        std::set<Address>::iterator iter = preList.begin();
+        std::set<Address>::const_iterator iter = preList.begin();
         out << *iter;
-        for (iter++; iter != preList.end(); iter++)
+        for (++iter; iter != preList.end(); ++iter)
             out << "; " << *iter;
     }
     return out;
